2c77881c1b91311a4f4e8835c1225eeb6f8b30d5
SUP2Ak <cormier.wesley@gmail.com>
2024-04-06 17:35:04 +0200

    refactor(fxmanifest): adapt for new process

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'cerulean'
-game 'gta5'
+games { 'gta5', 'rdr3' } --- not tested on rdr3
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
 
-client_script 'init.lua'
-
 files {
-    'imports/**.lua',
-    'obj.lua'
+    'init.lua',
+    'src/**',
+    'src/**/**'
 }
+
+client_script 'src/main.lua'
