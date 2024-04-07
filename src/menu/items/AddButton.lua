91542063143f5231b7bf7c6061fd7bdaeebbbf47
SUP2Ak <cormier.wesley@gmail.com>
2024-04-08 00:02:17 +0200

    refactor(menu/items.button): add config, start logic

--- src/menu/items/AddButton.lua
+++ src/menu/items/AddButton.lua
@@ -1,6 +1,19 @@
-return function(self, menu, test, test2)
+local config <const> = require '@sublime_nativeui.config.menu.button'
+
+---@param self Items
+---@param menu Menu
+---@param label string
+---@param description string
+---@return integer buttonId
+return function(self, menu, label, description)
     menu.size += 1
     --print(test, test2, menu.size)
+
+    if menu.index == menu.size then
+        print('isActive', menu.index, menu.size)
+    else
+        print('isNotActive', menu.index, menu.size)
+    end
 
     return menu.size --[[@as buttonId]] --[[@return integer]]
 end
