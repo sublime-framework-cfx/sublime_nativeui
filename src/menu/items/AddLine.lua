29949cf8d5b71326db2339374db375c190c0a683
SUP2Ak <cormier.wesley@gmail.com>
2024-04-08 00:02:30 +0200

    refactor(menu/items.line): add config, start logic

--- src/menu/items/AddLine.lua
+++ src/menu/items/AddLine.lua
@@ -1,3 +1,20 @@
-return function ()
-    print('line xD')
+-- local config <const> = require '@sublime_nativeui.config.menu.line'
+
+---@param self Items
+---@param menu Menu
+---@param styles table
+return function(self, menu, styles)
+    menu.size += 1
+
+    if menu.index == menu.size then -- is active so go to next
+        if menu.lastIndex > menu.index then -- if last index is greater than current index, we up
+            menu.index -= 1 -- decrement index to up
+        elseif menu.lastIndex < menu.index then -- if last index is less than current index, we down
+            menu.index += 1 -- increment index to down
+        end
+    end
+
+    if styles and (type(styles) == 'table' and table.type(styles) == 'hash') then -- avoid error
+        ---@todo personalize line style, color, animation, etc ...
+    end
 end
