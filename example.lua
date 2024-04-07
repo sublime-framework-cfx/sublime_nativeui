136f49f2de3d03d444658251fa2fb138730a61b3
SUP2Ak <cormier.wesley@gmail.com>
2024-04-07 21:54:35 +0200

    refactor(example): new example for v0.0.2

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local menu = nativeui:new({
+local menu = nativeui.CreateMenu:new({
     id = 'main',
     title = 'Main Menu',
     subtitle = 'subtitle',
@@ -6,19 +6,31 @@ local menu = nativeui:new({
     width = .2,
 })
 
-local sub = menu:AddSubMenu({
+local sub = nativeui.CreateMenu:new({
+    parent = 'main',
     id = 'sub',
-    title = 'Sub Menu',
+    title = 'Main Menu',
     subtitle = 'subtitle',
     backgroundColor = {0, 255, 255, 125},
     width = .1,
 })
 
+-- local sub = menu:AddSubMenu({
+--     id = 'sub',
+--     title = 'Sub Menu',
+--     subtitle = 'subtitle',
+--     backgroundColor = {0, 255, 255, 125},
+--     width = .1,
+-- })
+
 local isAsync = 'mon button 1'
 
-function menu:pool(item)
-    item:AddButton(isAsync, 'et ouais')
-    local button2 = item:AddButton('mon button 2', 'et ouais')
+function menu:pool(item, panel)
+    item:AddButton(self, isAsync, 'et ouais')
+    local button2 = item:AddButton(self, 'mon button 2', 'et ouais', sub)
+
+    -- panel:Slider(self, button2, 'mon slider', 0, 100, 50, 1, 'et ouais')
+    -- panel:Slider(self, button2, 'mon slider 2', 0, 100, 50, 1, 'et ouais')
 
     if IsControlJustPressed(0, 38) then -- E
         print('here?')
@@ -27,10 +39,14 @@ function menu:pool(item)
 end
 
 function sub:pool(item)
-    item:AddButton('mon button 3', 'et ouais')
-    item:AddButton('mon button 4', 'et ouais')
-    print('here?')
+    item:AddButton(self, 'mon button 3', 'et ouais')
+    item:AddButton(self, 'mon button 4', 'et ouais')
+    --print('here?')
 end
+
+RegisterCommand('oplast', function()
+    nativeui.OpenLastMenu()
+end)
 
 RegisterCommand('isA', function()
     if isAsync == 'mon button 1' then
@@ -41,7 +57,7 @@ RegisterCommand('isA', function()
 end)
 
 RegisterCommand('suop', function()
-    if menu:isOpen() then
+    if menu:IsOpen() then
         menu:Close()
     else
         menu:Open()
@@ -49,5 +65,5 @@ RegisterCommand('suop', function()
 end)
 
 RegisterCommand('cco', function()
-    nativeui.CloseAll()
+    nativeui.CloseMenu()
 end)
