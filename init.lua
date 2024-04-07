265a7c2b5740a9eb4cfaf33167f85fbd98067c7e
SUP2Ak <cormier.wesley@gmail.com>
2024-04-07 23:37:02 +0200

    refactor(init): check rss object have re write require method before rewrite it and clear code on load module

--- init.lua
+++ init.lua
@@ -16,132 +16,139 @@ end
 
 local export <const> = exports[s_ui]
 
+-- [fr]: Pas besoin de réécrire la fonction require si un des ses objets existe | [en]: No need to rewrite the require function if one of its objects exists 
+if not supv and not lib and not sublime then
+    local loaded = {}
+    package = {
+        loaded = setmetatable({}, {
+            __index = loaded,
+            __newindex = function()  end,
+            __metatable = false,
+        }),
+        path = './?.lua;'
+    }
 
-local loaded = {}
-package = not lib and {
-    loaded = setmetatable({}, {
-        __index = loaded,
-        __newindex = function()  end,
-        __metatable = false,
-    }),
-    path = './?.lua;'
-} or package
+    local _require = require
 
-local _require = require
+    ---@param modname string
+    ---@return unknown?
+    local function LoadModule(modname)
+        if type(modname) ~= 'string' then return end
 
----@param modname string
----@return unknown?
-local function LoadModule(modname)
-    if type(modname) ~= 'string' then return end
+        local module = loaded[modname]
 
-    local module = loaded[modname]
-
-    if not module then
-        if module == false then
-            error(("^1circular-dependency occurred when loading module '%s'^0"):format(modname), 2)
-        end
-
-        if not modname:find('^@') then
-            local success, result = pcall(_require, modname)
-
-            if success then
-                loaded[modname] = result
-                return result
+        if not module then
+            if module == false then
+                error(("^1circular-dependency occurred when loading module '%s'^0"):format(modname), 2)
             end
 
-            local modpath = modname:gsub('%.', '/')
+            if not modname:find('^@') then
+                local success, result = pcall(_require, modname)
 
-            for path in package.path:gmatch('[^;]+') do
-                local scriptPath = path:gsub('?', modpath):gsub('%.+%/+', '')
-                local resourceFile = LoadResourceFile(supv.env, scriptPath)
+                if success then
+                    loaded[modname] = result
+                    return result
+                end
 
-                if resourceFile then
-                    loaded[modname] = false
-                    scriptPath = ('@@%s/%s'):format(supv.env, scriptPath)
+                local modpath = modname:gsub('%.', '/')
 
-                    local chunk, err = load(resourceFile, scriptPath)
+                for path in package.path:gmatch('[^;]+') do
+                    local scriptPath = path:gsub('?', modpath):gsub('%.+%/+', '')
+                    local resourceFile = LoadResourceFile(nativeui.env, scriptPath)
 
-                    if err or not chunk then
-                        loaded[modname] = nil
-                        return error(err or ("unable to load module '%s'"):format(modname), 3)
+                    if resourceFile then
+                        loaded[modname] = false
+                        scriptPath = ('@@%s/%s'):format(nativeui.env, scriptPath)
+
+                        local chunk, err = load(resourceFile, scriptPath)
+
+                        if err or not chunk then
+                            loaded[modname] = nil
+                            return error(err or ("unable to load module '%s'"):format(modname), 3)
+                        end
+
+                        module = chunk(modname) or true
+                        loaded[modname] = module
+
+                        return module
+                    end
+                end
+            else
+                local rss, dir = modname:gsub('%.', '/'):match('^(.-)/(.+)$')
+
+                if not rss or not dir then return error('Invalid path format: '..modname, 2) end
+                rss, dir = rss:gsub('^@', ''), dir..'.lua'
+                local chunk = LoadResourceFile(rss, dir)
+
+                if chunk then
+                    local scriptPath = ('@@%s/%s'):format(rss, dir)
+                    local func, err = load(chunk, scriptPath)
+
+                    if err or not func then
+                        return error(err or ("unable to load module '%s'"):format(modname), 2)
                     end
 
-                    module = chunk(modname) or true
+                    module = func(modname) or true
                     loaded[modname] = module
 
                     return module
                 end
             end
-        else
-            local rss, dir = modname:gsub('%.', '/'):match('^(.-)/(.+)$')
 
-            if not rss or not dir then return error('Invalid path format: '..modname, 2) end
-            rss, dir = rss:gsub('^@', ''), dir..'.lua'
-            local chunk = LoadResourceFile(rss, dir)
-
-            if chunk then
-                local scriptPath = ('@@%s/%s'):format(rss, dir)
-                local func, err = load(chunk, scriptPath)
-
-                if err or not func then
-                    return error(err or ("unable to load module '%s'"):format(modname), 2)
-                end
-
-                module = func(modname) or true
-                loaded[modname] = module
-
-                return module
-            end
+            return error(("module '%s' not found"):format(modname), 2)
         end
 
-        return error(("module '%s' not found"):format(modname), 2)
+        return module
     end
 
-    return module
+    require = LoadModule
 end
 
-_r = LoadModule
-
-local function void() end
-local function load_module(self, index)
-    if self.exportsMethod[index] then return end
-    local func, err, dir, data
-
-    for k, v in pairs(self.module) do
-        dir = v..'/'..index
-        data = LoadResourceFile(s_ui, dir..'.lua')
-        if data then break end
-    end
-
-    if not data then return end
-
-    local chunk <const> = data
-
-    if chunk then
-        func, err = load(chunk, ('@@%s/%s'):format(s_ui, dir))       
-        if err then error(("Error to loading modules :\n- From : %s\n - Modules : %s\n - Service : %s\n - Error : %s"):format(dir, index, service, err), 3) end
-
-        local result = func()
-        rawset(self, index, result or void)
-        return self[index]
-    end
+if not supv and not sublime then
+    local function void() end
+    _ENV.void = void
+else
+    _ENV.void = void
 end
 
 local function call_module(self, index, ...)
     local module = rawget(self, index)
+    -- print('module', module, index, ...)
+
     if not module then
         self[index] = void
-        module = load_module(self, index)
-        if not module then
+
+        if self.exportsMethod[index] then
             local function method(...)
                 return export[index](nil, ...)
             end
+
             if not ... then
                 self[index] = method
             end
+
             return method
+        else
+            local path <const> = export:GetPathModule(index)
+            if not path then
+                error(('module %s not found'):format(index), 2)
+                return 
+            end
+
+            module = require(path)
+            
+            --[[@? no need that for now
+                if not ... then
+                    module = require(path)
+                else
+                    module = require(path)(...)
+                end
+            --]]
+
+            rawset(self, index, module)
         end
     end
+
     return module
 end
 
@@ -150,17 +157,9 @@ local nativeui = setmetatable({
     current = '',
     exportsMethod = export:GetExportMethod(),
     env = GetCurrentResourceName(),
-    module = {
-        --items = 'src/items', ceci ne compte pas comme des module de nativeui
-        --panels = 'src/panels', ceci ne compte pas comme des module de nativeui
-
-        -- il y aura plus un module menupause, scaleform, progressbar etc...
-        menu = 'src/menu',
-    }
 }, {
     __index = call_module,
     __call = call_module,
 })
 
 _ENV.nativeui = nativeui
-_ENV.void = void
