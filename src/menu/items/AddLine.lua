e5b2396e35c85ff3ab368981b31e1a5dc763bf8c
SUP2Ak <cormier.wesley@gmail.com>
2024-04-09 23:57:08 +0200

    feat(menu.items): init AddLine file

--- src/menu/items/AddLine.lua
+++ src/menu/items/AddLine.lua
@@ -4,17 +4,5 @@
 ---@param menu Menu
 ---@param styles table
 return function(self, menu, styles)
-    menu.size += 1
-
-    if menu.index == menu.size then -- is active so go to next
-        if menu.lastIndex > menu.index then -- if last index is greater than current index, we up
-            menu.index -= 1 -- decrement index to up
-        elseif menu.lastIndex < menu.index then -- if last index is less than current index, we down
-            menu.index += 1 -- increment index to down
-        end
-    end
-
-    if styles and (type(styles) == 'table' and table.type(styles) == 'hash') then
-        ---@todo personalize line style, color, animation, etc ...
-    end
+    ---@todo
 end
