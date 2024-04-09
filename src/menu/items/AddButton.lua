6ec792576f303d4bdf19e4c7a43a491a95398f2f
SUP2Ak <cormier.wesley@gmail.com>
2024-04-09 23:56:37 +0200

    feat(menu.items): AddButton principal "example"

--- src/menu/items/AddButton.lua
+++ src/menu/items/AddButton.lua
@@ -4,26 +4,42 @@ local config <const> = require '@sublime_nativeui.config.menu.button'
 ---@param menu Menu
 ---@param label string
 ---@param description string
+---@param options table
+---@param actions table
+---@param nextMenu table | string
 ---@return integer buttonId
-return function(self, menu, label, description)
-    menu.size += 1
-    --print(test, test2, menu.size)
+return function(self, menu, label, description, options, actions, nextMenu)
+    menu.counter += 1
+    self.id = menu.counter
+    self.actions = actions or self.actions
 
-    if menu.index == menu.size then
-        menu.currentDescription = description
-        --print('isActive', menu.index, menu.size)
+    if (menu.counter < menu.pagination.min) or (menu.counter > menu.pagination.max ) then
+        if self.actions.onExit then
+            self:IsLastActive(menu) 
+        end
     else
-        --print('isNotActive', menu.index, menu.size)
-    end
+        local y <const> = menu:GetY(config.h)
+        if self:IsActive(menu) then
+            DrawRect(menu.x, y + menu.offsetY, menu.w, config.h, 255, 255, 255, 255)
+            menu.currentDescription = description
+            if IsControlJustPressed(0, 18) then
+                if self.actions.onSelected then
+                    self.actions.onSelected(self, menu)
+                    PlaySoundFrontend(-1, "SELECT", "HUD_LIQUOR_STORE_SOUNDSET", true)
+                end
 
-    menu.offset += config.h + menu.marginItem
-    if menu.size == 1 then
-        DrawRect(menu.x, menu.y + menu.offset, menu.w, config.h, 0, 0, 255, 75)
-    elseif menu.size == 2 then
-        DrawRect(menu.x, menu.y + menu.offset, menu.w, config.h, 0, 255, 0, 50)
-    else
-        DrawRect(menu.x, menu.y + menu.offset, menu.w, config.h, 255, 0, 0, 50)
-    end
+                if nextMenu then
+                    menu:GoTo(nextMenu)
+                end
+            end
+        else
+            DrawRect(menu.x, y + menu.offsetY, menu.w, config.h, 0, 0, 0, 120)
+            if self.actions.onExit then
+                self:IsLastActive(menu) 
+            end
+        end
 
-    return menu.size --[[@as buttonId]] --[[@return integer]]
+        menu.offsetY += (config.h + menu.padding)
+    end
+    return self.id
 end
