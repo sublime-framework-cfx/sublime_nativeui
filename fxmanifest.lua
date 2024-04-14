40f10c09d78165d5f8dfd41d11dd1a2d87e48e0b
SUP2Ak <cormier.wesley@gmail.com>
2024-04-14 04:52:45 +0200

    bump(fxmanifest): v0.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,13 @@ fx_version 'cerulean'
 games { 'gta5', 'rdr3' } --- not tested on rdr3
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
-version '0.0.4'
+version '0.1.0'
 
 files {
     'config/**',
     'src/**',
     'src/**/**',
+    'src/**/**/**',
     'init.lua',
 }
 
