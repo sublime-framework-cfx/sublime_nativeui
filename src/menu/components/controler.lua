e964d30305c54b56368208372357a21349d9b378
SUP2Ak <cormier.wesley@gmail.com>
2024-04-14 04:49:36 +0200

    fix(menu/c.controle): correct speed and smooth nav

--- src/menu/components/controler.lua
+++ src/menu/components/controler.lua
@@ -1,61 +1,60 @@
 local step, time, pressed = 0, GetGameTimer(), false
 
 local function GoUp(menu)
-    if IsControlPressed(0, 172) then -- UP
-        local newIndex <const> = menu.index - 1
+    local newIndex <const> = menu.index - 1
 
-        if newIndex < 1 then
-            menu.index = menu.counter
-            
-            menu.pagination.min = menu.counter - menu.maxVisibleItems + 1
-            menu.pagination.max = menu.counter
-        else
-            if menu.index == menu.pagination.min then
-                menu.pagination.min -= 1
-                menu.pagination.max = menu.pagination.min + menu.maxVisibleItems - 1
-            end
-            menu.index = newIndex
+    if newIndex < 1 then
+        menu.index = menu.counter
+        
+        menu.pagination.min = menu.counter - menu.maxVisibleItems + 1
+        menu.pagination.max = menu.counter
+    else
+        if menu.index == menu.pagination.min then
+            menu.pagination.min -= 1
+            menu.pagination.max = menu.pagination.min + menu.maxVisibleItems - 1
         end
-
-        PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
-        time, pressed = GetGameTimer(), true
-        step += 1
+        menu.index = newIndex
     end
+
+    PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+    time, pressed = GetGameTimer(), true
+    step += 1
 end
 
 local function GoDown(menu)
-    if IsControlPressed(0, 173) then -- DOWN      
-        local newIndex <const> = menu.index + 1
+    local newIndex <const> = menu.index + 1
 
-        if newIndex > menu.counter then
-            menu.index = 1
-            menu.pagination.min = 1
-            menu.pagination.max = menu.maxVisibleItems
-        else
-            if menu.index == menu.pagination.max then
-                menu.pagination.min += 1
-                menu.pagination.max = menu.pagination.min + menu.maxVisibleItems - 1
-            end
-
-            menu.index = newIndex
+    if newIndex > menu.counter then
+        menu.index = 1
+        menu.pagination.min = 1
+        menu.pagination.max = menu.maxVisibleItems
+    else
+        if menu.index == menu.pagination.max then
+            menu.pagination.min += 1
+            menu.pagination.max = menu.pagination.min + menu.maxVisibleItems - 1
         end
 
-        PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
-        time, pressed = GetGameTimer(), true
-        step += 1
+        menu.index = newIndex
     end
+
+    PlaySoundFrontend(-1, 'NAV_UP_DOWN', 'HUD_FRONTEND_DEFAULT_SOUNDSET', true)
+    time, pressed = GetGameTimer(), true
+    step += 1
 end
 
 local function Controler(menu)
     pressed = false
 
-    if menu.closable and IsControlJustPressed(0, 177) then -- BACKSPACE
-        menu:Close()
+    if menu.closable and IsControlJustPressed(0, 194) then -- BACKSPACE
+        menu:Close('GoBack')
     end
 
-    if (step < 3 and (GetGameTimer() - time > 450) or (step < 6 and GetGameTimer() - time > 350)) or (GetGameTimer() - time > 50) then
-        GoUp(menu)
-        GoDown(menu)
+    if (step < 3 and (GetGameTimer() - time > 150) or (step < 6 and step >= 3 and GetGameTimer() - time > 75)) or (step >= 6 and GetGameTimer() - time > 45) then
+        if IsControlPressed(0, 172) then -- UP
+            GoUp(menu)
+        elseif IsControlPressed(0, 173) then -- DOWN    
+            GoDown(menu)
+        end
     else
         pressed = true
     end
@@ -64,4 +63,4 @@ local function Controler(menu)
     if not pressed then step = 0 end
 end
 
-return Controler
+return Controler, GoUp, GoDown
