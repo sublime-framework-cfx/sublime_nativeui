3b5c33c2b7fd43431148c607a46375ff36f42137
SUP2Ak <cormier.wesley@gmail.com>
2024-04-14 04:48:35 +0200

    ci(config): menu & button

--- config/menu/button.lua
+++ config/menu/button.lua
@@ -4,4 +4,9 @@
 
 return {
     h = 0.025,
+    animation = {
+        enabled = true,
+        type = 'sneaky',--'progress-join',
+        color = { 200, 200, 200, 150 },
+    },
 }
--- config/menu/global.lua
+++ config/menu/global.lua
@@ -9,10 +9,14 @@ return {
     subtitle = 'Selected', --[ [nil / false if you don't want subtitle ]]
     subtitleH = .03,
     glare = true,
-    background = false,
+    background = true,
     maxVisibleItems = 12,
     pagination = { min = 1 --[[ don't touch this! ]], max = 12 --[[@as maxVisibleItems]] },
     default = { x = .035, y = .045, w = .225, h = .2 --[[@unknown ]] },
     padding = .0025, -- padding between all elements (banner, subtitle, items, etc...)
     backgroundColor = { 0, 0, 0, 100 }, --[[@as rgba]]
+    animation = {
+        enabled = true,
+        type = 'default',
+    },
 }
