4c059b3fb53424ec4c392a38d19e84e5da190b9f
SUP2Ak <cormier.wesley@gmail.com>
2024-04-22 00:38:06 +0200

    fix(config/menu.global): remove pagination from config because all menu is this ref

--- config/menu/global.lua
+++ config/menu/global.lua
@@ -11,7 +11,6 @@ return {
     glare = true,
     background = true,
     maxVisibleItems = 12,
-    pagination = { min = 1 --[[ don't touch this! ]], max = 12 --[[@as maxVisibleItems]] },
     default = { x = .035, y = .045, w = .225, h = .2 --[[@unknown ]] },
     padding = .0025, -- padding between all elements (banner, subtitle, items, etc...)
     backgroundColor = { 0, 0, 0, 100 }, --[[@as rgba]]
