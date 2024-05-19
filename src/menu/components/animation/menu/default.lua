621c1a0b9b033296dfca3fdd258a3cf8f7a5695b
SUP2Ak <cormier.wesley@gmail.com>
2024-05-19 23:31:29 +0200

    chore(animation.menu): animation global for menu is load in menu folder

---
+++ src/menu/components/animation/menu/default.lua
@@ -0,0 +1,63 @@
+local animation = {
+    config = {
+        open = {
+            y = -.4
+        },
+        close = {}
+    }
+}
+
+function animation.open(menu, config)
+    local endY = menu.defaultY
+    local currentY = animation.config.open.y
+    menu.y = currentY
+
+    local startTime = GetGameTimer()
+    local duration = 250
+
+    while currentY < endY do
+        local currentTime = GetGameTimer()
+        local elapsedTime = currentTime - startTime
+        local percentComplete = elapsedTime / duration
+
+        if percentComplete >= 1 then
+            menu:SetPosition(nil, endY)
+            break
+        end
+
+        currentY = animation.config.open.y + percentComplete * (endY + math.abs(animation.config.open.y))
+        menu:SetPosition(nil, currentY)
+
+        Wait(0)
+    end
+
+    menu.playAnimation = false
+    return true
+end
+
+function animation.close(menu, config)
+    local endY = -.5
+    local currentY = menu.y
+    local startTime = GetGameTimer()
+    local duration = 250
+
+    while currentY > endY do
+        local currentTime = GetGameTimer()
+        local elapsedTime = currentTime - startTime
+        local percentComplete = elapsedTime / duration
+
+        if percentComplete >= 1 then
+            menu:SetPosition(nil, endY)
+            break
+        end
+
+        currentY = menu.y - percentComplete * (menu.y + 0.5)
+        menu:SetPosition(nil, currentY)
+        Wait(0)
+    end
+
+    menu.playAnimation = false
+    return true
+end
+
+return animation
